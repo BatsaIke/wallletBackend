ea3a56f7b4794fb4e4c847af858070cb45b965c1
IsaacKimo <139249129+IsaacKimo@users.noreply.github.com>
2024-02-26 15:00:52 +0000

    push to vercel

--- server.js
+++ server.js
@@ -1,54 +1,36 @@
 const express = require("express");
-const connectDB = require('./db/db.js')
-const dotenv = require('dotenv'); 
+const connectDB = require('./db/db.js');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const usersDetails = require("./routes/userRoute.js");
-// const userPosts = require("./routes/postRoute.js");
 const userProfile = require("./routes/profileRoute");
 const userAuth = require("./routes/authRoute.js");
 const payStack = require("./routes/paystackRoute.js");
 const cors = require("cors");
-const customCorsMiddleware = require("./utils/corsCustomMiddleware.js");
 
 const app = express();
 dotenv.config();
 
-
-
-
-
-// app.use(cors({
-//   origin: '*', // or use a function to dynamically set the origin
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
-//   credentials: true,
-// }));
-
-//connect database
+// Connect to the database
 connectDB();
-//init middleware
+
+// Init middleware
 app.use(express.json({ extended: false }));
-app.use(cors(customCorsMiddleware));
+app.use(cors()); // Enable CORS for all origins
 app.use(bodyParser.json());
-//define routes
+
+// Define routes
 app.use("/api/v1/user", usersDetails);
 app.use("/api/v1/payment", payStack);
 app.use("/api/v1/password", userProfile);
-// app.use("/api/v1/profile", userProfile);
 app.use("/api/v1/auth/user", userAuth);
 
+const PORT = process.env.PORT || 5100;
+app.get("/api/v1", (req, res) => res.send("API is running"));
+const server = app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
 
-const PORT = process.env.PORT ||5100;
-app.get("/", (req, res) => res.send("Api is running"));
-const server=app.listen(PORT, () => console.log(`Servver is listening on port: ${PORT}`));
- 
- 
-//unhandled promise rejection
+// Handle unhandled promise rejections
 process.on('unhandledRejection', (reason, promise) => {
     console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-    // Handle the error appropriately, e.g., close the application or log the error.
-    server.close(()=>{
-        process.exit(1);  
-    })
-  });
-   
+    server.close(() => process.exit(1));
+});
