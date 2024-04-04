7844e610e9a2ea546c246aaed70eb86d6cbaf331
IsaacKimo <139249129+IsaacKimo@users.noreply.github.com>
2024-04-04 12:16:39 +0000

    modify url

--- products/src/api.js
+++ products/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
