e0eb019ad045386723e81f395f0f4c2c22bab313
IsaacKimo <139249129+IsaacKimo@users.noreply.github.com>
2024-04-30 18:01:24 +0000

    add product fix

--- products/src/components/hooks/useProducts.js
+++ products/src/components/hooks/useProducts.js
@@ -1,33 +1,15 @@
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchProducts } from '../../actions/productActions';
-
-// export const useProducts = (queryParams) => {
-//   const dispatch = useDispatch();
-//   const { products, loading, error } = useSelector(state => state.product);
-
-//   useEffect(() => {
-//     console.log("Fetching data directly from API");
-//     dispatch(fetchProducts(queryParams));
-//   }, [dispatch, JSON.stringify(queryParams)]); // Serialize queryParams to ensure stable dependency
-
-//   return { productsData: products, loading, error };
-// };
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProducts } from '../../actions/productActions';
 
 export const useProducts = (queryParams) => {
   const dispatch = useDispatch();
-  const { products, loading, error } = useSelector(state => state.product);
+  const { products, loading, error } = useSelector((state) => state.product);
 
   useEffect(() => {
-    // Check if products already exist in the store
-    if (!products.length) {
-      console.log("Fetching data directly from API");
-      dispatch(fetchProducts(queryParams));
-    }
-  }, [dispatch, JSON.stringify(queryParams), products.length]); // Include products.length in dependency array
+    console.log("Fetching data directly from API");
+    dispatch(fetchProducts(queryParams));
+  }, [dispatch, JSON.stringify(queryParams)]); // Serialize queryParams to ensure stable dependency
 
   return { productsData: products, loading, error };
 };
