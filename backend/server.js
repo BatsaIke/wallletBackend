17b21575f4ed8e01afa3afbf3eebb0b0db838eee
IsaacKimo <139249129+IsaacKimo@users.noreply.github.com>
2024-05-03 23:26:59 +0000

    add product fix

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const app = express();
 const PORT = process.env.PORT || 5100;
 
 // Middleware
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../products/build")));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.json());
 app.use(cors()); // Enable CORS for all origins
